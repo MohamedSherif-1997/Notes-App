093677968ae3beb3daafbaefbb22f86932ba9b65
Sherif <sherifkhan1997@gmail.com>
2020-06-10 15:00:24 +0530

    created a dashBoard Drawer

---
+++ src/constants/string.js
@@ -0,0 +1,2 @@
+export const DRAWER_PRIMARY_LIST = ["My Notes", "Create Notes"];
+export const DRAWER_SECONDARY_LIST = ["All Notes", "Trash", "Spam"];
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
-
+import { Grid } from "@material-ui/core";
+import Header from "../../components/header";
+import DashboardDrawer from "../../components/drawer";
 class Dashboard extends Component {
   render() {
-    return <h1>Dashboard</h1>;
+    return (
+      <Grid container>
+        <Header />
+        <DashboardDrawer />
+      </Grid>
+    );
   }
 }
 export default Dashboard;
